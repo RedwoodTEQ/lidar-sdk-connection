bd9586f938fffffecb6bc408e3d7f3476ae02d2a
TrollTown <williamyinanshen2009@gmail.com>
2021-02-26 17:08:56 +1100

    Add roscore start up step

--- data_decode/rosbag_decode_roscore/new_entrypoint.sh
+++ data_decode/rosbag_decode_roscore/new_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-. /opt/ros/melodic/setup.sh && roscore &
+
+. /opt/ros/melodic/setup.sh && roscore -p 11311 &
 . /opt/ros/melodic/setup.sh && cd /storage && rosbag play rslidar_points*.bag
 . /opt/ros/melodic/setup.sh && rostopic echo /rslidar_points > /output/point_cloud.out
