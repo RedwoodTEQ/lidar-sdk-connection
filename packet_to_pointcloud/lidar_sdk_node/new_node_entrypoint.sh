7602a8eaeb971858566508ec6559a8701d503e55
TrollTown <williamyinanshen2009@gmail.com>
2021-06-10 01:51:50 +1000

    Add conversion

---
+++ packet_to_pointcloud/lidar_sdk_node/new_node_entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. /opt/ros/melodic/setup.sh && \
+echo $ROS_MASTER_URI && \
+./home/rslidar_sdk/build/rslidar_sdk_node
---
+++ packet_to_pointcloud/roscore/new_entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. /opt/ros/melodic/setup.sh && roscore -p 11311 &
+. /opt/ros/melodic/setup.sh && cd /storage && rosbag play rslidar_packet_0.bag rslidar_packet_1.bag rslidar_packet_2.bag rslidar_packet_3.bag rslidar_packet_4.bag rslidar_packet_5.bag rslidar_packet_6.bag rslidar_packet_7.bag &
+. /opt/ros/melodic/setup.sh && cd /storage && rosbag record --split --duration=30 /rslidar_points -O rslidar_points
