451f4aec9748bfe5b54762c85ae7d929a1f22890
William Yi Nan Shen <williamyinanshen2009@gmail.com>
2021-09-16 23:19:11 +1000

    Remove decoding step

--- data_decode/rosbag_decode_roscore/new_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. /opt/ros/melodic/setup.sh && python /rosbag_decode.py
-
-# . /opt/ros/melodic/setup.sh && roscore -p 11311 &
-
-# . /opt/ros/melodic/setup.sh && cd /storage && rosbag play rslidar_points*.bag
-# . /opt/ros/melodic/setup.sh && rostopic echo /rslidar_points > /output/point_cloud.out
