1aebf14ec09652baa8212c5fe5c5bb84d3779fdb
William Yi Nan Shen <williamyinanshen2009@gmail.com>
2021-09-24 00:41:06 +1000

    read pcd from archive and convert to istream

--- processing/test_visualise.cpp
+++ processing/test_visualise.cpp
@@ -1,10 +1,14 @@
 #include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <istream>
+#include <streambuf>
+#include <string>
 #include <thread>
 #include <vector>
 
 #include <boost/filesystem.hpp>
+#include <boost/interprocess/streams/bufferstream.hpp>
 
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/filter.h>
@@ -24,6 +28,13 @@
 using namespace std::literals::chrono_literals;
 
 # define M_PI           3.14159265358979323846  /* pi */
+
+struct membuf : std::streambuf
+{
+    membuf(char* begin, char* end) {
+        this->setg(begin, begin, end);
+    }
+};
 
 pcl::visualization::PCLVisualizer::Ptr mapping_vis (pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
 {
@@ -131,6 +142,15 @@ int main () {
   }
   std::cout << "====================" << std::endl;
 
+  pcl::visualization::PCLVisualizer::Ptr viewer;
+  viewer = mapping_vis(display_cloud);
+  viewer->addLine(pcl::PointXYZ(0,18.35,0), pcl::PointXYZ(50,-24.15,0), 255, 0, 0, std::string("median_divider_line"), 0);
+  viewer->addLine(pcl::PointXYZ(-5,18.75,0), pcl::PointXYZ(50,-29,0), 255, 0, 0, std::string("lane_2_3_divider_line"), 0);
+  viewer->addLine(pcl::PointXYZ(-7,16.25,0), pcl::PointXYZ(50,-34,0), 255, 0, 0, std::string("lane_1_2_divider_line"), 0);
+  viewer->addLine(pcl::PointXYZ(-9,14,0), pcl::PointXYZ(50,-39,0), 255, 0, 0, std::string("road_boundary_line"), 0);
+  viewer->addLine(pcl::PointXYZ(3.9,-10,0), pcl::PointXYZ(20.9,10,0), 0, 255, 0, std::string("far_cut_off_line"), 0);
+  viewer->addLine(pcl::PointXYZ(1.4025,20,0), pcl::PointXYZ(-15.5975,0,0), 0, 255, 0, std::string("close_cut_off_line"), 0);
+
   // Open test data archive
   // How to read file within archives: https://stackoverflow.com/questions/31500340/how-to-read-file-within-archives/31500763
   struct archive *a;
@@ -141,43 +161,11 @@ int main () {
   a = archive_read_new();
   archive_read_support_filter_gzip(a);
   archive_read_support_format_tar(a);
-  r = archive_read_open_filename(a, "westmead_pcd/lidar0.pcd.gz", 10240);
+  r = archive_read_open_filename(a, "westmead_pcd/lidar0.pcd.gz", 1036288);
   if (r != ARCHIVE_OK){
     return 1;
   }
   while (archive_read_next_header(a, &a_entry) == ARCHIVE_OK) {
-    auto file_size = archive_entry_size(a_entry);
-    auto buffer = new char[file_size];
-    std::cout << "libarchive: " << archive_entry_pathname(a_entry) << std::endl;
-
-    auto read_result = archive_read_data(a, buffer, file_size);
-    if (read_result != ARCHIVE_OK) {
-      std::cout << "after1.1" << std::endl;
-      return 1;
-    }
-    // std::cout << buffer << std::endl;
-    std::cout << "after" << std::endl;
-    break;
-  }
-  r = archive_read_free(a);
-  if (r != ARCHIVE_OK) {
-    return 1;
-  }
-  return 0;
-
-
-  std::vector<boost::filesystem::path> dir_files;
-
-  pcl::visualization::PCLVisualizer::Ptr viewer;
-  viewer = mapping_vis(display_cloud);
-  viewer->addLine(pcl::PointXYZ(0,18.35,0), pcl::PointXYZ(50,-24.15,0), 255, 0, 0, std::string("median_divider_line"), 0);
-  viewer->addLine(pcl::PointXYZ(-5,18.75,0), pcl::PointXYZ(50,-29,0), 255, 0, 0, std::string("lane_2_3_divider_line"), 0);
-  viewer->addLine(pcl::PointXYZ(-7,16.25,0), pcl::PointXYZ(50,-34,0), 255, 0, 0, std::string("lane_1_2_divider_line"), 0);
-  viewer->addLine(pcl::PointXYZ(-9,14,0), pcl::PointXYZ(50,-39,0), 255, 0, 0, std::string("road_boundary_line"), 0);
-  viewer->addLine(pcl::PointXYZ(3.9,-10,0), pcl::PointXYZ(20.9,10,0), 0, 255, 0, std::string("far_cut_off_line"), 0);
-  viewer->addLine(pcl::PointXYZ(1.4025,20,0), pcl::PointXYZ(-15.5975,0,0), 0, 255, 0, std::string("close_cut_off_line"), 0);
-  // viewer->addLine(pcl::PointXYZ(6.0,-2.97,0), pcl::PointXYZ(25.0,25.0,0), 0, 0, 255, std::string("test_line"), 0);
-  for (const boost::filesystem::path & filename : dir_files) {
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZI>);
     pcl::PointCloud<pcl::PointXYZI>::Ptr passThroughIn (new pcl::PointCloud<pcl::PointXYZI>);
     pcl::PointCloud<pcl::PointXYZI>::Ptr passThroughBetter (new pcl::PointCloud<pcl::PointXYZI>);
@@ -185,103 +173,23 @@ int main () {
     pcl::PointCloud<pcl::PointXYZI>::Ptr bgBetter (new pcl::PointCloud<pcl::PointXYZI>);
     pcl::PointCloud<pcl::PointXYZI>::Ptr clusterBetter (new pcl::PointCloud<pcl::PointXYZI>);
 
+    const auto fsize = archive_entry_size(a_entry);
+    std::cout << fsize << std::endl;
+    auto buffer = new char[fsize];
+    std::cout << "libarchive: " << archive_entry_pathname(a_entry) << std::endl;
+    auto read_result = archive_read_data(a, buffer, fsize);
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZI> (filename.string(), *cloud) == -1) {
-      PCL_ERROR ("Couldn't read pcd file \n");
-      return (-1);
-    }
-    std::cout << filename.string() << std::endl;
-    std::vector<int> indices;
-    pcl::removeNaNFromPointCloud(*cloud, *passThroughIn, indices); // this shouldn't affect anything but doing it anyway as it will make it easier to process data later
-
-    if (pass_through_filtering){
-      pcl::PassThrough<pcl::PointXYZI> x_filter; 
-      x_filter.setInputCloud(passThroughIn);
-      x_filter.setFilterFieldName("x");
-      x_filter.setFilterLimits(filter_x_min, filter_x_max);
-      x_filter.filter(*passThroughBetter);
-
-      passThroughIn->clear();
-
-      pcl::PassThrough<pcl::PointXYZI> z_filter;
-      z_filter.setInputCloud(passThroughBetter);
-      z_filter.setFilterFieldName("z");
-      z_filter.setFilterLimits(filter_z_min, filter_z_max);
-      z_filter.filter(*passThroughIn);
-
-      passThroughBetter->clear();
-
-      pcl::PassThrough<pcl::PointXYZI> y_filter;
-      z_filter.setInputCloud(passThroughIn);
-      z_filter.setFilterFieldName("y");
-      z_filter.setFilterLimits(filter_y_min, filter_y_max);
-      z_filter.filter(*passThroughBetter);
-    }
-    else {
-      pcl::copyPointCloud(*passThroughIn, *passThroughBetter);
+    // Ensure number of bytes read from the current pcd file in archive matches its actual size
+    if (read_result != fsize) {
+      return 1;
     }
 
-    if (box_filtering){
-      pcl::CropBox<pcl::PointXYZI> box_filter;
-      std::vector<int> indices2;
-      box_filter.setInputCloud(passThroughBetter);
-      Eigen::Vector4f min_pt (0.0f, 0.0f, -20.0f, 1);
-      Eigen::Vector4f max_pt (7.7f, 21.341f, 20.0f, 1);
-      box_filter.setMin (min_pt);
-      box_filter.setMax (max_pt);
-      box_filter.setTransform(pcl::getTransformation(-5.0f, 8.4f, 0.0f, 0.0f, 0.0f, -41.9872 * M_PI / 180.0));
-      box_filter.filter(indices2);
-      box_filter.filter(*boxBetter);
-    }
-    else {
-      pcl::copyPointCloud(*passThroughBetter, *boxBetter);
-    }
-    
+    // Convert char array to istream
+    membuf sbuf(buffer, buffer + fsize);
+    std::istream dataStream(&sbuf);
+    std::cout << dataStream.rdbuf();
 
-    if (bg_filtering) {
-      pcl::SegmentDifferences<pcl::PointXYZI> difference_segmenter;
-      difference_segmenter.setInputCloud(boxBetter);
-      difference_segmenter.setTargetCloud(calib_cloud);
-      difference_segmenter.setDistanceThreshold(0.5);
-      difference_segmenter.segment(*bgBetter);  
-    } 
-    else {
-      pcl::copyPointCloud(*boxBetter, *bgBetter);
-    }
-    std::cout << "Points in frame: " << bgBetter->size() << std::endl;
-    // // Clustering test
-
-    if (bgBetter->size() > 0){
-      pcl::PointCloud<pcl::PointXYZINormal>::Ptr bgBetter_with_normals (new pcl::PointCloud<pcl::PointXYZINormal>);
-      pcl::search::KdTree<pcl::PointXYZI>::Ptr search_tree (new pcl::search::KdTree<pcl::PointXYZI>);
-      pcl::IndicesClustersPtr clusters (new pcl::IndicesClusters);
-      pcl::copyPointCloud(*bgBetter, *bgBetter_with_normals);
-      pcl::NormalEstimation<pcl::PointXYZI, pcl::PointXYZINormal> ne;
-      ne.setInputCloud(bgBetter);
-      ne.setSearchMethod(search_tree);
-      ne.setRadiusSearch(1.0);
-      ne.compute(*bgBetter_with_normals);
-      pcl::ConditionalEuclideanClustering<pcl::PointXYZINormal> cec (true);
-      cec.setInputCloud(bgBetter_with_normals);
-      cec.setConditionFunction(&customRegionGrowing);
-      cec.setClusterTolerance(1.0);
-      cec.setMinClusterSize(50);
-      cec.segment(*clusters);
-
-      for (int i = 0; i < clusters->size (); ++i)
-      {
-        int label = rand () % 8;
-        for (int j = 0; j < (*clusters)[i].indices.size (); ++j)
-          (*bgBetter)[(*clusters)[i].indices[j]].intensity = label;
-      }
-      std::cout << clusters->size() << " vehicles in frame" << std::endl;
-      std::cout << "==================" << std::endl;
-    } else {
-      std::cout << "0 vehicles in frame" << std::endl;
-    }
-    
-    viewer->updatePointCloud<pcl::PointXYZI>(bgBetter, "sample cloud");
-    viewer->spinOnce(50);
+    break;
   }
-  return (0);
+  return 0;
 }
