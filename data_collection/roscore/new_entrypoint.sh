686efeff3634adbd749eba8072785834746270db
William Yi Nan Shen <williamyinanshen2009@gmail.com>
2021-10-17 15:33:54 +1100

    collection log test

--- data_collection/roscore/new_entrypoint.sh
+++ data_collection/roscore/new_entrypoint.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-echo "ROS core entrypoint loaded..."
-echo $$
+# Logging function from https://stackoverflow.com/questions/48086633/simple-logging-levels-in-bash/48087251
+declare -A levels=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3)
+script_logging_level="INFO"
+
+logThis() {
+    local log_message=$1
+    local log_priority=$2
+
+    #check if level exists
+    [[ ${levels[$log_priority]} ]] || return 1
+
+    #check if level is enough
+    (( ${levels[$log_priority]} < ${levels[$script_logging_level]} )) && return 2
+
+    #log here
+    echo "${log_priority} : ${log_message}"
+}
+
+
+logThis "ROS core entrypoint loaded..." "INFO"
+logThis "{$$}" "INFO"
+
+collection_log() {
+    while :; do
+        cd /sharedstorage
+        du -h *.bag
+        du -hs .
+        sleep 30
+    done
+}
 
 _term () {
     echo "Caught SIGTERM signal!"
@@ -32,6 +60,9 @@ archive () {
 trap _term SIGTERM TERM
 echo "ABCDEF"
 
+collection_log &
+collection_log_pid=$!
+
 . /opt/ros/melodic/setup.sh && \
 roscore -p 11311 &
 . /opt/ros/melodic/setup.sh && \
@@ -40,6 +71,7 @@ rosbag record --split --duration=30 /rslidar_points -O rslidar_points &
 
 child=$!
 wait "$child"
+kill $collection_log_pid
 
 
 
