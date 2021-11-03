30ed382dfd969f61ef107a26f14e7a3811cba5fa
William Yi Nan Shen <williamyinanshen2009@gmail.com>
2021-11-04 00:37:37 +1100

    Add uuid generator

--- processing/test_visualise.cpp
+++ processing/test_visualise.cpp
@@ -112,6 +112,26 @@ typedef struct _bounding_box {
   double delta_z;
 } BoundingBox, *BoundingBoxPtr;
 
+// Simple "uuid" generator by CaptainCodeman
+// https://stackoverflow.com/questions/24365331/how-can-i-generate-uuid-in-c-without-using-boost-library/58467162
+string get_uuid() {
+    static random_device dev;
+    static mt19937 rng(dev());
+
+    uniform_int_distribution<int> dist(0, 15);
+
+    const char *v = "0123456789abcdef";
+    const bool dash[] = { 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 };
+
+    string res;
+    for (int i = 0; i < 16; i++) {
+        if (dash[i]) res += "-";
+        res += v[dist(rng)];
+        res += v[dist(rng)];
+    }
+    return res;
+}
+
 BoundingBox determineBoundingBox(pcl::PointCloud<pcl::PointXYZI>::Ptr cluster_cloud) {
   BoundingBox bounding_box = {};
   pcl::PointXYZ baseline = pcl::PointXYZ(-5.98, 11.31, 0);
